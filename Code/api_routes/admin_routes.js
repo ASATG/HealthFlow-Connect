97620b5ee2a486d57d758341d7fd1708f10b5b41
ASATG <ayushambhorkar@gmail.com>
2024-03-01 14:42:31 -0800

    Adding the functions to create, update and delete counter, pharmacist and lab technician to admin controller and necessary routes into admin_router

--- Code/api_routes/admin_routes.js
+++ Code/api_routes/admin_routes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { add_doctor_record, update_doctor_record, delete_doctor_record } from "../api_controllers/admin_controllers.js";
+import { add_doctor_record, update_doctor_record, delete_doctor_record, add_counter_record, delete_counter_record, add_pharmacist_record, delete_pharmacist_record, add_lab_technician_record, delete_lab_technician_record, update_otherstaff_record } from "../api_controllers/admin_controllers.js";
 
 const admin_router = express.Router();
 
 admin_router.post('/add_doctor_record/', add_doctor_record);
 admin_router.post('/update_doctor_record/', update_doctor_record);
 admin_router.post('/delete_doctor_record/', delete_doctor_record);
+admin_router.post('/add_counter_record/', add_counter_record);
+admin_router.post('/delete_counter_record/', delete_counter_record);
+admin_router.post('/add_pharmacist_record/', add_pharmacist_record);
+admin_router.post('/delete_pharmacist_record/', delete_pharmacist_record);
+admin_router.post('/add_lab_technician_record/', add_lab_technician_record);;
+admin_router.post('/delete_lab_technician_record/', delete_lab_technician_record);
+admin_router.post('/update_otherstaff_record/', update_otherstaff_record);
 
 export { admin_router };
