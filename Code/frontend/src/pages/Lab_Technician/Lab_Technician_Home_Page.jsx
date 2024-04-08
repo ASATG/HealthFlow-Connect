9381272867767c863329426527dd400c6377048d
Vipul <vipulchaudhari.3131@gmail.com>
2024-04-08 11:51:43 +0530

    Styling done to pages of Lab Technicians and improved navigation

--- Code/frontend/src/pages/Lab_Technician/Lab_Technician_Home_Page.jsx
+++ Code/frontend/src/pages/Lab_Technician/Lab_Technician_Home_Page.jsx
@@ -36,7 +36,7 @@ export const Lab_Technician_Home_Page = () => {
         const helper_id = event.target.id;
         const target_div_id = "div_" + helper_id;
         const target_div = document.getElementById(target_div_id);
-        const patient_id = target_div.childNodes[5].textContent.split(":")[1].slice(1);
+        const patient_id = target_div.childNodes[5].textContent.split(":")[1];
 
         let api_call = await axios.post("http://localhost:3500/get_phone_number_from_uid", { u_id: patient_id });
         if (api_call.data.success_status) {
@@ -80,32 +80,197 @@ export const Lab_Technician_Home_Page = () => {
     };
 
     return (
-        <Fragment>
-            <h1>This is Lab Technician Home Page</h1>
-            <button onClick={(e) => navigator("/logout/", { replace: true })}>Logout</button>
-            <Personal_Info_Component explicit_keys_to_exclude={[]} record={person_info} />
-            <Personal_Info_Component explicit_keys_to_exclude={["person_id"]} record={other_info} />
-            <hr />
-            {unserved_redirection_records.map(record => (
-                <div id={"div_" + record.redirection_record_id_string}>
-                    <p>Redirection Creation Date Time: {record.redirection_creation_date_time}</p>
-                    <p>Is Redirection Served: {record.is_redirection_served ? 'Yes' : 'No'}</p>
-                    <p>Staff ID: {record.staff_u_id}</p>
-                    <p>Staff Name: {record.staff_name}</p>
-                    <p>Staff Designation: {record.staff_designation}</p>
-                    <p>Patient ID: {record.patient_u_id}</p>
-                    <p>Patient Name: {record.patient_name}</p>
-                    <p>Lab Testings To Be Done:</p>
-                    <ul>
+        // <Fragment>
+        <div className="doctor-home-container">
+            <div class="content">
+        <h1>This is Lab Technician Home Page</h1>
+        <button className="logout-button" onClick={(e) => navigator("/logout/", { replace: true })}>
+          Logout
+        </button>
+        </div>
+        <div>
+        <div className="sidebar-doctor">
+        <p className="p_doctor">Lab Work</p>
+        <div className="records-container">
+        {unserved_redirection_records.map((record) => (
+            <div className="served-patients" id={"div_" + record.redirection_record_id_string}>
+              <hr />
+              <div class="form-group">
+                <label>Redirection Creation Date Time:</label>
+                <p>{record.redirection_creation_date_time}</p>
+              </div>
+              <div class="form-group">
+                <label>Is Redirection Served:{" "}</label>
+                <p>{record.is_redirection_served ? "Yes" : "No"}</p>
+              </div>
+              <div class="form-group">
+                <label>Staff ID:</label>
+                <p>{record.staff_u_id}</p>
+              </div>
+              <div class="form-group">
+                <label>Staff Designation:</label>
+                <p>{record.staff_designation}</p>
+              </div>
+              <div class="form-group">
+                <label>Patient ID:</label>
+                <p>{record.patient_u_id}</p>
+              </div>
+              <div class="form-group">
+                <label>Patient Name:</label>
+                <p>{record.patient_name}</p>
+              </div>
+              <div class="form-group">
+              <label>Lab Testings To Be Done:</label>
                         {record.lab_testings_to_be_done.map((test, index) => (
-                            <li key={index}>{test}</li>
+                            <p key={index}>{test}</p>
                         ))}
-                    </ul>
-                    <button onClick={handle_redirection_serve_btn} id={record.redirection_record_id_string}>Serve</button>
-                    <hr />
-                </div>
-            ))
-            }
-        </Fragment >
+              
+              </div>
+              <button
+                class="btn1"
+                onClick={handle_redirection_serve_btn}
+                id={record.redirection_record_id_string}
+              >
+                Serve
+              </button>
+              <hr />
+            </div>
+          ))}
+        </div>
+
+        </div>
+        <div className="main-container">
+          <h2>Doctor Information</h2>
+          <div className="personal-info">
+            <div>
+              <p>UID</p>
+              <p>{person_info["u_id"]}</p>
+            </div>
+            <div>
+              <p>First Name</p>
+              <p>{person_info["first_name"]}</p>
+            </div>
+            <div>
+              <p>Middle Name</p>
+              <p>{person_info["middle_name"]}</p>
+            </div>
+            <div>
+              <p>Last Name</p>
+              <p>{person_info["last_name"]}</p>
+            </div>
+            <div>
+              <p>Gender</p>
+              <p>{person_info["gender"]}</p>
+            </div>
+            <div>
+              <p>Date of Birth</p>
+              <p>{person_info["dob"]}</p>
+            </div>
+            <div>
+              <p>Phone Number</p>
+              <p>{person_info["phone_number"]}</p>
+            </div>
+            <div>
+              <p>Address</p>
+              <p>{person_info["address"]}</p>
+            </div>
+            <div>
+              <p>Role</p>
+              <p>{person_info["role"]}</p>
+            </div>
+            <div>
+              <p>Degree</p>
+              <p>{other_info["degree"]}</p>
+            </div>
+            <div>
+              <p>Specialization</p>
+              <p>{other_info["specialization"]}</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+
+
+            // <h1>This is Lab Technician Home Page</h1>
+            // <button onClick={(e) => navigator("/logout/", { replace: true })}>Logout</button>
+
+            // { <div className="main-container">
+          /*<h2>Admin Information</h2>
+          <div className="personal-info">
+            <div>
+              <p>UID</p>
+              <p>{personInfo["u_id"]}</p>
+            </div>
+            <div>
+              <p>First Name</p>
+              <p>{personInfo["first_name"]}</p>
+            </div>
+            <div>
+              <p>Middle Name</p>
+              <p>{personInfo["middle_name"]}</p>
+            </div>
+            <div>
+              <p>Last Name</p>
+              <p>{personInfo["last_name"]}</p>
+            </div>
+            <div>
+              <p>Gender</p>
+              <p>{personInfo["gender"]}</p>
+            </div>
+            <div>
+              <p>Date of Birth</p>
+              <p>{personInfo["dob"]}</p>
+            </div>
+            <div>
+              <p>Phone Number</p>
+              <p>{personInfo["phone_number"]}</p>
+            </div>
+            <div>
+              <p>Address</p>
+              <p>{personInfo["address"]}</p>
+            </div>
+            <div>
+              <p>Role</p>
+              <p>{personInfo["role"]}</p>
+            </div>
+            <div>
+              <p>OPD</p>
+              <p>{adminInfo["opd"]}</p>
+            </div>
+            <div>
+              <p>Degree</p>
+              <p>{adminInfo["degree"]}</p>
+            </div>
+            <div>
+              <p>Specialization</p>
+              <p>{adminInfo["specialization"]}</p>
+            </div>
+          </div>
+        </div> */
+            // <Personal_Info_Component explicit_keys_to_exclude={[]} record={person_info} />
+            // <Personal_Info_Component explicit_keys_to_exclude={["person_id"]} record={other_info} />
+            // <hr />
+            // {unserved_redirection_records.map(record => (
+            //     <div id={"div_" + record.redirection_record_id_string}>
+            //         <p>Redirection Creation Date Time: {record.redirection_creation_date_time}</p>
+            //         <p>Is Redirection Served: {record.is_redirection_served ? 'Yes' : 'No'}</p>
+            //         <p>Staff ID: {record.staff_u_id}</p>
+            //         <p>Staff Name: {record.staff_name}</p>
+            //         <p>Staff Designation: {record.staff_designation}</p>
+            //         <p>Patient ID: {record.patient_u_id}</p>
+            //         <p>Patient Name: {record.patient_name}</p>
+            //         <p>Lab Testings To Be Done:</p>
+            //         <ul>
+            //             {record.lab_testings_to_be_done.map((test, index) => (
+            //                 <li key={index}>{test}</li>
+            //             ))}
+            //         </ul>
+            //         <button onClick={handle_redirection_serve_btn} id={record.redirection_record_id_string}>Serve</button>
+            //         <hr />
+            //     </div>
+            // ))
+            // }
+        // </Fragment >
     );
 }
